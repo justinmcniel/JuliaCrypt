de37dc601701e78d11a15c50bb1d4d04bbc88843
Justin McNiel <jmcnieltu@gmail.com>
2026-01-22 13:55:48 -0600

    Project Creation and Original UI Mockup

---
+++ ViewModels/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace JuliaCrypt
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
